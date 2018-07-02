d775303fc195322899be4503b6ad5a75fa44629d
Fernando Infanzon <luisfernandoim@gmail.com>
2018-07-02 00:14:04 -0500

    first commit

---
+++ server/miiddlewares/autenticacion.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+
+// Verificacion Token
+
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+
+    });
+}
+
+
+module.exports = {
+    verificaToken
+}
